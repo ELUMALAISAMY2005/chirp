f7134e5878bd3d9b65b625f05665b7fd186ed3d2
ELUMALAISAMY2005 <sec22it079@sairamtap.edu.in>
2024-11-21 00:08:27 +0530

    image

--- chirp/wxui/main.py
+++ chirp/wxui/main.py
@@ -540,7 +540,7 @@ class ChirpMain(wx.Frame):
         if sys.platform == 'win32':
             icon = 'chirp.ico'
         else:
-            icon = 'chirp.png'
+            icon = 'URIMAIGAL.png'
         with resources.as_file(
             resources.files('chirp.share').joinpath(icon)
         ) as path:
